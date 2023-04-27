cbb667dce86978ef625f329a5502315e4e7b628a
IA Automator <dev@null>
2023-04-27 21:39:02 +0000

    (automated) Updates from project type

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
+
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
 # managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
@@ -7,25 +8,21 @@ PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 
 cd ${PROJECT_PATH}
 
-export AWS_DEFAULT_REGION='us-east-1'
-
-aws ec2 describe-regions --query 'Regions[?OptInStatus==`opt-in-not-required`].RegionName' --output text | xargs -n 1 | while read dn
+regions=(us-east-1 us-east-2 us-west-2 us-west-1)
+for region in ${regions[@]}
 do
-    echo "Cleanup running in region: $dn"
-    export AWS_DEFAULT_REGION=$dn
+    echo "Cleanup running in region: $region"
+    export AWS_DEFAULT_REGION=$region
     python3 scripts/cleanup_config.py -C scripts/cleanup_config.json
 done
 
+echo $AWS_DEFAULT_REGION
 unset AWS_DEFAULT_REGION
 
+echo $AWS_DEFAULT_REGION
 # Run taskcat e2e test
 taskcat test run
 
-
-#export AWS_DEFAULT_REGION=us-east-1
-#ls -l ${PROJECT_PATH}/scripts/cleanup_config.py
-#ls -l ${PROJECT_PATH}/scripts/cleanup_config.json 
-#python3 scripts/cleanup_config.py -C scripts/cleanup_config.json
 ## Executing ash tool
 
 #find ${PROJECT_PATH} -name lambda.zip -exec rm -rf {} \;
