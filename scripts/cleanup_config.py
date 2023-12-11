4a909eed2696b56781a73325ad7c17486b2b6926
Kishore Vinjam <kkvinjam@gmail.com>
2023-12-11 17:58:38 -0500

    Added SRA submodule at tag v3.0.4 and removed ABI submodules

--- scripts/cleanup_config.py
+++ scripts/cleanup_config.py
@@ -13,7 +13,7 @@ LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
 SESSION = boto3.session.Session()
-print('Region: %s', SESSION.region_name)
+print(f"Region: {SESSION.region_name}")
 
 CF = SESSION.client('cloudformation')
 SSM = SESSION.client('ssm')
@@ -32,6 +32,14 @@ def list_stacksets():
         response = CF.list_stack_sets(NextToken=response['NextToken'])
         stacksets.extend(response['Summaries'])
     return stacksets
+
+def list_active_stackset_names():
+    '''List all stackset names in the account'''
+    cf_names = []
+    for cfn in list_stacksets():
+        if cfn['Status'] != 'DELETED':
+            cf_names += [cfn['StackSetName']]
+    return cf_names
 
 def list_stackset_names(filters=None):
     '''List all stackset names in the account'''
@@ -82,16 +90,36 @@ def si_region_list(stackset_name):
         stackinstance_regions += [stackinstance['Region']]
     return stackinstance_regions
 
+def get_ou_list(account_list):
+    '''
+    Return list of ous for provided list of accounts
+    '''
+    ou_list = []
+    for account in account_list:
+        ou_id = ORG.list_parents(ChildId=account)['Parents'][0]['Id']
+        ou_list.append(ou_id)
+    return list(set((ou_list)))
+
 def delete_all_stackinstances(stackset_name):
     '''Delete a stackset'''
     response = None
+    perm_model = CF.describe_stack_set(StackSetName=stackset_name)['StackSet']['PermissionModel']
     account_list = list(dict.fromkeys(si_account_list(stackset_name)))
+    ou_list = get_ou_list(account_list)
     region_list = list(dict.fromkeys(si_region_list(stackset_name)))
     if len(account_list) != 0 and len(region_list) != 0:
-        response = CF.delete_stack_instances(StackSetName=stackset_name,
-                            Regions=region_list,
-                            Accounts=account_list,
-                            RetainStacks=False)
+        if perm_model == 'SERVICE_MANAGED':
+            print(f"Deleting {stackset_name} in {region_list} with {ou_list}")
+            response = CF.delete_stack_instances(StackSetName=stackset_name,
+                        Regions=region_list, DeploymentTargets={'OrganizationalUnitIds': ou_list},
+                        RetainStacks=False)
+        else:
+            print(f"Deleting {stackset_name} in {region_list} with {account_list}")
+            response = CF.delete_stack_instances(StackSetName=stackset_name,
+                                                 Regions=region_list,
+                                                 Accounts=account_list,
+                                                 RetainStacks=False)
+
         loop = 1
         while len(list_stackset_instances(stackset_name)) > 0 and loop < 30:
             sleep(10)
@@ -149,71 +177,79 @@ def delete_stack(filters='tCaT-'):
         stack_name = stack['StackName']
         stack_status = stack['StackStatus']
         if stack_name.startswith(filters) and stack_status != 'DELETE_COMPLETE':
-            if not is_nested_stack(stack_name):
-                print('Deleting stack: %s', stack_name)
-                CF.delete_stack(StackName=stack_name)
-                wait = 1
-                while list_stack_status_by_name(stack_name) not in STACKSTATUS and wait < 60:
-                    print('Wait: %s, Stack: %s', stack_name, wait)
-                    sleep(10)
-                    wait += 1
+            print('Deleting stack: %s', stack_name)
+            CF.delete_stack(StackName=stack_name)
+            wait = 1
+            while list_stack_status_by_name(stack_name) not in STACKSTATUS and wait < 60:
+                print('Wait: %s, Stack: %s', stack_name, wait)
+                sleep(10)
+                wait += 1
 
 def delete_all_objects_from_s3_bucket(bucket_name, account=None):
     '''Delete all objects from an S3 bucket'''
     if account:
-        session = establish_remote_session(account)
-        sss = session.resource('s3')
+        sss_session = establish_remote_session(account)
+        type(sss_session)
+        sss = sss_session.resource('s3')
     else:
         sss = boto3.resource('s3')
 
     bucket = sss.Bucket(bucket_name)
 
-    print('Deleting all objects from bucket: %s', bucket_name)
+    print('..Deleting all objects from bucket: %s', bucket_name)
     bucket.object_versions.delete()
     bucket.objects.all().delete()
 
-def delete_s3_buckets(filters='sra-staging-', account=None):
+def delete_s3_buckets(item):
     '''Delete all S3 buckets created by CfCT solution in the account'''
-    if account:
-        session = establish_remote_session(account)
-        sss = session.client('s3')
-    else:
-        sss = boto3.client('s3')
+
+    filters=item['Filter']
+    (sss, account, target) = get_client_session(item, 's3')
+
+    print(f"S3 action on {target} with filters: {filters}")
 
     response = sss.list_buckets()
     buckets = response['Buckets']
+
     while response.get('NextToken'):
         response = sss.list_buckets(NextToken=response['NextToken'])
         buckets.extend(response['Buckets'])
-        print(buckets)
+
     for bucket in buckets:
-        if bucket['Name'].startswith(filters):
-            print(filters)
+        bucket_name = bucket['Name']
+        if bucket_name.startswith(filters):
             try:
-                delete_all_objects_from_s3_bucket(bucket['Name'], account)
-                print('Deleting bucket: %s', bucket['Name'])
-                sss.delete_bucket(Bucket=bucket['Name'])
+                delete_all_objects_from_s3_bucket(bucket_name, account)
+                print(f"....Deleting bucket: {bucket_name}")
+                sss.delete_bucket(Bucket=bucket_name)
             except Exception as exe:
                 if exe.response['Error']['Code'] == 'NoSuchBucket':
-                    print('S3 bucket deletion issue. Skipping: %s', bucket['Name'])
+                    print(f"S3 bucket deletion issue. Skipping: {bucket_name}")
                 else:
                     raise exe
 
-def list_all_parameters():
+def list_all_parameters(ssm_session=SSM):
     ''''List all parameters in the account'''
-    response = SSM.describe_parameters()
+    response = ssm_session.describe_parameters()
     parameters = response['Parameters']
     while response.get('NextToken'):
-        response = SSM.describe_parameters(NextToken=response['NextToken'])
+        response = ssm_session.describe_parameters(NextToken=response['NextToken'])
         parameters.extend(response['Parameters'])
     return parameters
 
-def delete_parameters(filters='/sra/'):
-    '''Delete all parameters created by CfCT solution in the account'''
-    parameters = list_all_parameters()
+def delete_parameters(item):
+    '''Delete all parameters created in the account'''
+
+    filters = item['Filter']
+    (ssm_session, account, target) = get_client_session(item, 'ssm')
+    print(f"SSM action on {target} with filters: {filters}")
+
+    parameters = list_all_parameters(ssm_session)
     for parameter in parameters:
-        if parameter['Name'].startswith(filters):
-            SSM.delete_parameter(Name=parameter['Name'])
+        param_name = parameter['Name']
+        if param_name.startswith(filters):
+            print(f"..Deleting parameter {param_name}.")
+            ssm_session.delete_parameter(Name=param_name)
 
 def get_temp_credentials(aws_account, role_name='AWSControlTowerExecution'):
     '''
@@ -299,23 +335,20 @@ def list_cw_lognames(context):
 
     return result
 
-def delete_cw_logs(filters='sra/sra-org-trail', account=None):
+def delete_cw_logs(item):
     '''
     Delete the pre-req data created during testing
     '''
-    if account:
-        session = establish_remote_session(account)
-        cwlogs = session.client('logs')
-        print('Account-ID: %s', account)
-    else:
-        cwlogs = SESSION.client('logs')
 
-    log_groups = list_cw_lognames(context=cwlogs)
-    print(filters)
+    filters = item['Filter']
+    (cw_session, account, target) = get_client_session(item, 'logs')
+    print(f"LOG GROUP action on {target} with filters: {filters}")
+
+    log_groups = list_cw_lognames(context=cw_session)
     for log_group_name in log_groups:
         if log_group_name.startswith(filters):
-            print('Deleting log group: %s', log_group_name)
-            cwlogs.delete_log_group(logGroupName=log_group_name)
+            print(f"..Deleting log group {log_group_name}.")
+            cw_session.delete_log_group(logGroupName=log_group_name)
 
 def get_management_account_id():
     '''
@@ -323,12 +356,12 @@ def get_management_account_id():
     '''
     return ORG.describe_organization()['Organization']['MasterAccountId']
 
-def get_list_of_detectors():
+def get_list_of_detectors(gd_client):
     '''
     Get list of GuardDuty detectors
     '''
     detectors = []
-    paginator = GD.get_paginator('list_detectors')
+    paginator = gd_client.get_paginator('list_detectors')
     for page in paginator.paginate():
         detectors += page['DetectorIds']
     return detectors
@@ -352,10 +385,90 @@ def delete_detector():
             gd_client = boto3.client('guardduty')
 
         if gd_client:
-            detector_ids = get_list_of_detectors()
+            detector_ids = get_list_of_detectors(gd_client)
             for det_id in detector_ids:
                 print('Deleting GuardDuty Detector in %s', account['Id'])
                 gd_client.delete_detector(DetectorId=det_id)
+
+def list_cb_projects():
+    '''
+    List all CodeBuild projects
+    '''
+    cb_session = SESSION.client('codebuild')
+    response = cb_session.list_projects()
+    projects = response['projects']
+    while response.get('nextToken'):
+        response = cb_session.list_projects(nextToken=response['nextToken'])
+        projects.extend(response['projects'])
+
+    return projects
+
+def delete_build_projects(filters='sra-codebuild-'):
+    '''
+    Delete the CodeBuild projects in all accounts in the organization in the current region
+    '''
+    projects = list_cb_projects()
+    for project in projects:
+        if project.startswith(filters):
+            print('Deleting CodeBuild project: %s', project)
+            cb_session = SESSION.client('codebuild')
+            cb_session.delete_project(name=project)
+
+def get_account_info(ss_name='AWSControlTowerLoggingResources'):
+    '''
+    List first stack instances in a stackset
+    '''
+    result = None
+
+    if ss_name in list_active_stackset_names():
+        instance = CF.list_stack_instances(StackSetName=ss_name)
+        account_id = instance['Summaries'][0]['Account']
+        for account in get_list_of_accounts():
+            if account['Id'] == account_id:
+                account_name = account['Name']
+        result = {'AccountName': account_name, 'AccountID': account_id}
+    return result
+
+def get_client_session(item, client_name):
+    '''
+    Return a session for parent or child
+    '''
+
+    account = None
+    if 'Account' in item:
+        if item['Account'] in ACCOUNTS:
+            account = get_account_id(ACCOUNTS[item['Account']])
+    if account:
+        session = establish_remote_session(account)
+        client_session = session.client(client_name)
+        target = account
+    else:
+        client_session = boto3.client(client_name)
+        target = STS.get_caller_identity()['Account']
+
+    return (client_session, account, target)
+
+def delete_iam_role(item):
+    '''
+    Delete an IAM role - complete role name must be provided
+    '''
+
+    role_name = item['Filter']
+    (iam_session, account, target) = get_client_session(item, 'iam')
+    print(f"IAM action on {target} with role_name: {role_name}")
+
+    try:
+        policies = iam_session.list_attached_role_policies(RoleName=role_name)
+        for policy in policies['AttachedPolicies']:
+            print(f"..Detaching policy {policy['PolicyArn']} from role {role_name}.")
+            iam_session.detach_role_policy(RoleName=role_name, PolicyArn=policy['PolicyArn'])
+        print(f"....Deleting role {role_name}.")
+        iam_session.delete_role(RoleName=role_name)
+    except Exception as exe:
+        if exe.response['Error']['Code'] == 'NoSuchEntity':
+            print(f"IAM Role {role_name} does not exist.")
+        else:
+            raise exe
 
 def run_cleanup(config):
     '''
@@ -365,23 +478,17 @@ def run_cleanup(config):
         if item['Type'] == 'STACK':
             delete_stack(filters=item['Filter'])
         elif item['Type'] == 'S3_BUCKET':
-            account_id = None
-            if 'Account' in item:
-                if item['Account'] in ACCOUNTS:
-                    account_id = get_account_id(ACCOUNTS[item['Account']])
-            print('Account-id: %s', account_id)
-            delete_s3_buckets(filters=item['Filter'], account=account_id)
+            delete_s3_buckets(item)
         elif item['Type'] == 'SSM_PARAMETER':
-            delete_parameters(filters=item['Filter'])
+            delete_parameters(item)
         elif item['Type'] == 'STACK_SET':
             delete_stacksets(filters=item['Filter'])
+        elif item['Type'] == 'IAM_ROLE':
+            delete_iam_role(item)
+        elif item['Type'] == 'CODE_BUILD':
+            delete_build_projects(filters=item['Filter'])
         elif item['Type'] == 'LOG_GROUP':
-            account_id = None
-            if 'Account' in item:
-                if item['Account'] in ACCOUNTS:
-                    account_id = get_account_id(ACCOUNTS[item['Account']])
-            print('Account-id: %s', account_id)
-            delete_cw_logs(filters=item['Filter'], account=account_id)
+            delete_cw_logs(item)
         elif item['Type'] == 'GUARDDUTY_DET':
             delete_detector()
         else:
@@ -395,9 +502,22 @@ if __name__ == '__main__':
     PARSER.add_argument("-C", "--config", default='cleanup_config.json',
                         help="Clear content from config")
 
-    ACCOUNTS = {"log_account": "Log Archive", "audit": "Audit"}
     ARGS = PARSER.parse_args()
 
+    LOG_ACCT_INFO = get_account_info('AWSControlTowerLoggingResources')
+    AUDIT_ACCT_INFO = get_account_info('AWSControlTowerSecurityResources')
+    if LOG_ACCT_INFO:
+        LOG_ACCT_NAME = LOG_ACCT_INFO['AccountName']
+    else:
+        LOG_ACCT_NAME = 'Log Archive'
+
+    if AUDIT_ACCT_INFO:
+        AUDIT_ACCT_NAME = AUDIT_ACCT_INFO['AccountName']
+    else:
+        AUDIT_ACCT_NAME = 'Audit'
+
+    ACCOUNTS = {"log_account": LOG_ACCT_NAME, "audit": AUDIT_ACCT_NAME}
+    print('Recieved Account Info: %s', ACCOUNTS)
     CLEAR_CFG = ARGS.config
 
     if isfile(CLEAR_CFG):
