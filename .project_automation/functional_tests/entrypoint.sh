ea7b0b2ec1b4f948ea9ad4da128c7f92984c79a2
Tony Bulding <44652148+tbulding@users.noreply.github.com>
2024-04-25 09:56:30 -0500

    Update entrypoint.sh

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 ## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
-# managed and local tasks always use these variables for the project and project type path.
+# managed and local tasks always use these variables for the project and project type path
 PROJECT_PATH=${BASE_PATH}/project
 PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 NON_CT_ENV="039084729647"
